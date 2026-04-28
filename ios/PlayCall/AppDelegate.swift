6c5831bddc9006cd02f4c6c3a72d9db9d271f884
Mohamad-Zoghbi <mohamad.zoghbi412@gmail.com>
2026-04-28 13:41:27 +0300

    rename the app to gamingo

--- ios/PlayCall/AppDelegate.swift
+++ ios/PlayCall/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "PlayCall",
+      withModuleName: "Gamingo",
       in: window,
       launchOptions: launchOptions
     )
